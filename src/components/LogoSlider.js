73b10ca21ab1193d649afb41943367d768895b90
Mohamed Gamal El Baroky <m.baroky@gmail.com>
2024-03-11 22:37:53 +0200

    new brands-slider style (ticker)

--- src/components/LogoSlider.js
+++ src/components/LogoSlider.js
@@ -4,43 +4,46 @@ import Slider from "react-slick";
 function LogoSlider({ logos }) {
   const [settings] = useState({
     dots: false,
+    pauseOnHover: false,
+    swipe: false,
     infinite: true,
-    speed: 500,
     slidesToShow: 8,
     slidesToScroll: 1,
     initialSlide: 0,
     autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: false,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+    autoplaySpeed: 0,
+    speed: 4000,
+    cssEase: "linear",
+    // responsive: [
+    //   {
+    //     breakpoint: 1024,
+    //     settings: {
+    //       slidesToShow: 5,
+    //       slidesToScroll: 1,
+    //       infinite: true,
+    //       dots: false,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 600,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 1,
+    //       initialSlide: 2,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1,
+    //     },
+    //   },
+    // ],
   });
   // Your component logic
   return (
-    <div>
+    <div className='logo-slider'>
       <Slider {...settings}>
         {logos.map((logo, index) => (
           <div
--- src/sections/Brands.js
+++ src/sections/Brands.js
@@ -6,14 +6,21 @@ function Brands({ data }) {
     <section
       id='brands'
       className=' py-5 mb-5'
-      style={{ overflowX: "hidden" }}>
-      <div className='container-fluid'>
+      style={{ overflow: "hidden" }}>
+      <div className='container-fluid pb-5'>
         <div className='row'>
-          <p className='text-center'>
+          <p className='text-center mb-5'>
             <strong>Trusted Brands</strong>
           </p>
-          <div className='col'>
-            {data ? <LogoSlider logos={data} /> : "loading"}
+          <div id='slider-wrapper' className='col py-5'>
+            <div className='slider-tiker-wrapper'>
+              <div className='slider-tiker'>
+                {data ? <LogoSlider logos={data} /> : "loading"}
+              </div>
+              <div className='slider-tiker'>
+                {data ? <LogoSlider logos={data} /> : "loading"}
+              </div>
+            </div>
           </div>
         </div>
       </div>
